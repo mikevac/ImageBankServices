0390af55865cc8f30ae2b2763427ab7a4d5331d0
Michael Vaccaro <mikevac@protonmail.com>
2024-08-05 21:21:06 -0400

    added an addition to the database

--- src/main/java/com/vac/main/controller/response/BaseResponse.java
+++ src/main/java/com/vac/main/controller/response/BaseResponse.java
@@ -5,6 +5,11 @@ import lombok.Setter;
 
 @Getter
 @Setter
+/**
+ * BaseResponse
+ * 
+ * All responses to the client require a response status.
+ */
 public class BaseResponse {
 
     private String responseStatus;
